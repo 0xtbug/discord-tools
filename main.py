d178823e01387cf1a789e9653bc67863183f17ef
0xtbug <tubagus.nabil19@gmail.com>
2023-01-17 14:20:58 +0700

    fix & Show count success and fail

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import requests
 import os
 import json
 import pyfiglet
+import pytz
 from termcolor import colored
 from colorama import Fore
 
@@ -59,10 +60,10 @@ os.system('cls')
 result = pyfiglet.figlet_format("""Discord Tools""", font = "slant"  )
 print (colored(result, 'yellow'))
 ip = requests.get('https://api.ipify.org').text
-x = datetime.datetime.now() 
+x = datetime.datetime.now(pytz.timezone("Asia/Jakarta"))
 print (colored('''Created by: YSA DEV - Recoded by 0xtbug''', 'cyan', attrs=['bold']))
 print (colored('============================================================', 'green', attrs=['bold']))
-print (colored(f"Ξ START           : {x} \nΞ Your IP         : {ip} ", 'green', attrs=['bold']))
+print (colored(f"Ξ START           : {x.strftime('%d-%m-%Y %H:%M:%S')} \nΞ Your IP         : {ip} ", 'green', attrs=['bold']))
 print (colored('============================================================ \n', 'green', attrs=['bold']))
 choicel = str(input("Choose language ID or EN: "))
 choicec = str(input("Remove message after send? (y/n): "))
@@ -72,14 +73,17 @@ delaytime = int(input("Enter time ex. 60 seconds = 1 minutes: "))
 async def lp(ctx,amount: int):
     await ctx.message.delete()
     msgsend = amount
+    success_count = 0
+    fail_count = 0
     print(f"\n{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Sending {Fore.WHITE}{msgsend}{Fore.LIGHTBLACK_EX} messages to {Fore.WHITE}{ctx.guild.name}{Fore.LIGHTBLACK_EX} server")
     print(f"{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Estimated Time: {Fore.WHITE}{scale(msgsend)}\n")
     while msgsend > 0:
         try:
             msgsend -= 1
-            print(f"{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Message sent! | Messages left to send: {Fore.WHITE}{msgsend} {Fore.LIGHTBLACK_EX}| Estimated Time: {Fore.WHITE}{scale(msgsend)}")
+            success_count += 1
+            print(f"{Fore.WHITE}[{Fore.GREEN}{x.strftime('%d-%m-%Y %H:%M:%S')}{Fore.WHITE}] {Fore.LIGHTBLACK_EX}Message sent! | Messages left to send: {Fore.WHITE}{msgsend} {Fore.LIGHTBLACK_EX}| Estimated Time: {Fore.WHITE}{scale(msgsend)}")
             if msgsend == 0:
-                print(f"\n{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}All messages was sent")
+                print(f"\n{Fore.WHITE}[{Fore.GREEN}{x.strftime('%d-%m-%Y %H:%M:%S')}{Fore.WHITE}] {Fore.LIGHTBLACK_EX}All messages was sent. Successful: {Fore.WHITE}{success_count} {Fore.LIGHTBLACK_EX}| Failed: {Fore.WHITE}{fail_count}")
             if choicel == "ID" or choicel == "id":
                 kata = read_file('kataid.txt')
                 output = random.choice(kata)
@@ -90,7 +94,8 @@ async def lp(ctx,amount: int):
                 output = "Invalid input"
             await ctx.send(output)
         except:
-            print(f"{Fore.WHITE}[ {Fore.RED}- {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Cannot send message {Fore.WHITE}#{msgsend}")
+            fail_count += 1
+            print(f"{Fore.WHITE}[{Fore.RED}{x.strftime('%d-%m-%Y %H:%M:%S')}{Fore.WHITE}] {Fore.LIGHTBLACK_EX}Cannot send message {Fore.WHITE}#{msgsend}")
             pass
         await asyncio.sleep(1)
         async for message in ctx.message.channel.history(limit=1).filter(lambda m: m.author == client.user).map(lambda m: m):
@@ -98,17 +103,16 @@ async def lp(ctx,amount: int):
                 if choicec == "y" or choicec == "Y":
                     await message.delete()
             except:
-                print(f"{Fore.WHITE}[ {Fore.RED}- {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Cannot delete message {Fore.WHITE}#{msgsend}")
+                print(f"{Fore.WHITE}[{Fore.RED}{x.strftime('%d-%m-%Y %H:%M:%S')}{Fore.WHITE}] {Fore.LIGHTBLACK_EX}Cannot delete message {Fore.WHITE}#{msgsend}")
                 pass
-        await asyncio.sleep(delaytime) #setdelayhere(s)
+        await asyncio.sleep(delaytime)
     return
-
 
 @client.event
 async def print_info():
     await client.wait_until_ready()
     print (colored('+===================== BOT START! ========================+', 'red', attrs=['bold']))
-    print(f"{Fore.WHITE}[ {Fore.GREEN}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Your username: {Fore.WHITE}{client.user.name}")
+    print(f"{Fore.WHITE}[ {Fore.GREEN}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Login as: {Fore.WHITE}{client.user.name}")
     print(f"{Fore.WHITE}[ {Fore.GREEN}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Write ON DISCORD: {Fore.WHITE}\n!lp <number of messages> to Start Level UP")
 client.loop.create_task(print_info())
 
